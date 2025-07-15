5cf4b427d5ff059a9256bfc0ac7505fe28f4e0f7
Cacaiooo <caiovlopes2@gmail.com>
2025-07-15 20:22:08 -0300

    novos exercicios

---
+++ aprendendoJava/src/com/caio/cursojava/pratica4/exercicio1.java
@@ -0,0 +1,29 @@
+package com.caio.cursojava.pratica4;
+
+import java.util.Scanner;
+
+public class exercicio1 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		Boolean sucessoLogin = false;
+		
+		do {
+			System.out.println("Digite seu nome de usuario: ");
+			String nomeUsuario = scan.nextLine();
+			
+			System.out.println("Digite sua senha: ");
+			String senhaUsuario = scan.nextLine();
+			
+			if(nomeUsuario.equalsIgnoreCase(senhaUsuario)) {
+				sucessoLogin = false;
+				System.out.println("Erro! Senha e nome de usuário se coincidem.");
+			}else {
+				sucessoLogin = true;
+				System.out.println("Sucesso!");
+			}
+		}while(!sucessoLogin);
+
+	}
+
+}
---
+++ aprendendoJava/src/com/caio/cursojava/pratica4/exercicio3.java
@@ -0,0 +1,13 @@
+package com.caio.cursojava.pratica4;
+
+public class exercicio3 {
+
+	public static void main(String[] args) {
+		
+		do {
+			
+		}while(a);
+
+	}
+
+}
