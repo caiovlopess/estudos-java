40742f7cdb4660fe93f3b679a0abbfd831d9e3f5
Cacaiooo <caiovlopes2@gmail.com>
2025-07-24 18:36:26 -0300

    matriz multidimensional
    Merge branch 'main' of https://github.com/caiovlopess/estudos-java into
    main

--- aprendendoJava/src/com/caio/cursojava/pratica6/exercicio2.java
+++ aprendendoJava/src/com/caio/cursojava/pratica6/exercicio2.java
@@ -9,31 +9,52 @@ public class exercicio2 {
 		Scanner scan = new Scanner(System.in);
 		
 		int[][] vetor = new int[10][10];
+		Random numeroAleatorio = new Random();
 		
 		for(int i= 0; i < vetor.length; i++) {
 			for(int j =0; j < vetor[i].length; j++) {
-				Random numeroAleatorio = new Random(9);
+				vetor[i][j] = numeroAleatorio.nextInt(9);
 			}
+		}
+		
+		for(int i= 0; i < vetor.length; i++) {
+			for(int j =0; j < vetor[i].length; j++) {
+				System.out.print(vetor[i][j] + " ");
+			}
+			System.out.println();
 		}
 		
 		int maiorQuintaLinha = 0;
-		int menorQuintaLinha = 0;
-		int maiorSetimaLinha = 0;
-		int menorSetimaLinha = 0;
-		for(int i= 0; i < vetor[5].length; i++) {
-			if (vetor[5][i] > maiorQuintaLinha) {
-				maiorQuintaLinha = vetor[5].length;
+		int menorQuintaLinha = 100;
+		int L5 = 5;
+		for(int i= 0; i < vetor[L5].length; i++) {
+			if (vetor[L5][i] > maiorQuintaLinha) {
+				maiorQuintaLinha = vetor[L5][i];
 			}
-		for(int j= 0; j < vetor[7].length; j++) {
-			if (vetor[7][i] > maiorSetimaLinha) {
-				maiorSetimaLinha = vetor[7][i];
-			}
-				
+			if (vetor[L5][i] < menorQuintaLinha) {
+				menorQuintaLinha = vetor[L5][i];
 			}
 		}
 		
+		int maiorSetimaLinha = 0;
+		int menorSetimaLinha = 100;
+		int L7 = 7;
+		for(int i= 0; i < vetor[L7].length; i++) {
+			if (vetor[L7][i] > maiorSetimaLinha) {
+				maiorSetimaLinha = vetor[L7][i];
+			}
+			if (vetor[L7][i] < menorSetimaLinha) {
+				menorSetimaLinha = vetor[L7][i];
+			}
+		}
 		
-
+		System.out.println("Maior valor da linha 5: " + maiorQuintaLinha);
+		System.out.println("Menor valor da linha 5: " + menorQuintaLinha);
+		System.out.println();
+		System.out.println("Maior valor da linha 7: " + maiorSetimaLinha);
+		System.out.println("Menor valor da linha 7: " + menorSetimaLinha);
+			}
+	
 	}
 
-}
+
